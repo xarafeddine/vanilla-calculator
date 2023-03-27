6876f6f8e855d61cc06caa886f110c3233fccba9
Mohamed Charaf Eddine Ait Ougrram <maitougrram@smb.sqli.com>
2023-03-27 16:08:59 +0000

    init

---
+++ index.js
@@ -0,0 +1,146 @@
+const $plus = document.getElementById("+");
+const $one = document.getElementById("1");
+const $two = document.getElementById("2");
+const $yourInputs = document.getElementById("yourInputs");
+const $results = document.getElementById("results");
+
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+// const buttons = document.querySelectorAll("button");
+const equals = document.querySelector(".equals");
+const AC = document.querySelector("#AC");
+
+let firstNumber = "";
+let lastNumber = "";
+let operation;
+let result = "";
+
+const handleNumberClick = (e) => {
+  const number = e.target.innerText;
+
+  if (number === "." && lastNumber.includes(".")) return;
+  // console.log(value)
+  lastNumber += number;
+  result += number;
+  showInputs(result);
+};
+
+const handleOperatorClick = (e) => {
+  const op = e.target.innerText;
+
+  if (lastNumber === "") return;
+  if (firstNumber !== "") {
+    compute();
+  }
+  operation = op;
+  firstNumber = lastNumber;
+  lastNumber = "";
+
+  //   console.log(firstNumber, lastNumber);
+
+  result += op;
+
+  showInputs(result);
+};
+
+// function handleClick(e) {
+//   const value = e.target.innerText;
+//   console.log(value);
+//   result += value;
+// }
+
+// buttons.forEach((number) => {
+//   number.addEventListener("click", (e) => {
+//     handleClick(e);
+//     showValue();
+//   });
+// });
+
+addEventListeners();
+
+function showInputs(value) {
+  console.log("firstNumber", firstNumber);
+  console.log("lastNumber", lastNumber);
+  console.log("operation", operation);
+  $yourInputs.innerText = value;
+}
+
+function addEventListeners() {
+  operators.forEach((op) => {
+    op.addEventListener("click", (e) => {
+      handleOperatorClick(e);
+    });
+  });
+
+  numbers.forEach((number) => {
+    number.addEventListener("click", (e) => {
+      handleNumberClick(e);
+    });
+  });
+
+  equals.addEventListener("click", () => {
+    compute();
+  });
+
+  AC.addEventListener("click", () => {
+    function clear() {
+      firstNumber = "";
+      lastNumber = "";
+      operation = null;
+      result = "";
+      showResults("");
+
+      showInputs(result);
+    }
+  });
+}
+
+// function caluculate(result) {
+//   const numbers = result.split(/[+\-\*%\/]/);
+//   const ops = [...result.match(/[+\-\*%\/]/g)];
+//   let rslt = 0;
+
+//   for (let i = 0; i < numbers.length - 1; i += 2) {
+//     numbers[i] + numbers[i + 1];
+//   }
+
+//   //   const ops = result.split(/\w+/);
+//   console.log(numbers, ops);
+// }
+
+function compute() {
+  let computation;
+  const prev = parseFloat(firstNumber);
+  const current = parseFloat(lastNumber);
+  console.log(prev, current);
+  if (isNaN(prev) || isNaN(current)) return;
+  switch (operation) {
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+    case "*":
+      computation = prev * current;
+      break;
+    case "÷":
+      computation = prev / current;
+      break;
+    default:
+      return;
+  }
+
+  console.log(computation);
+  lastNumber = computation;
+  operation = undefined;
+  firstNumber = "";
+  result = computation;
+  showInputs();
+  showResults(computation);
+  return computation;
+}
+
+function showResults(vlue) {
+  $results.innerText = vlue;
+}
